a2ac847b812bc5b5a0b0b21b569166b435fd1137
Riajul Islam <rjriajulislam9@gmail.com>
2025-11-26 21:23:26 +0600

    done my CL tool

--- file-organizer.js
+++ file-organizer.js
@@ -28,60 +28,122 @@ const categories = {
 };
 
 const testFiels = [
-    "vacation.jpg",
-    "report.pdf",
-    "presentation.pptx",
-    "music.mp3",
-    "video.mp4",
-    "script.js",
-    "data.csv",
-    "archive.zip",
-    "photo.png",
-    "notes.txt",
-    "app.py",
-    "movie.avi",
-    "song.wav",
-    "backup.tar.gz",
-    "random.xyz",
-    "nodejs.zip"
+  "vacation.jpg",
+  "report.pdf",
+  "presentation.pptx",
+  "music.mp3",
+  "video.mp4",
+  "script.js",
+  "data.csv",
+  "archive.zip",
+  "photo.png",
+  "notes.txt",
+  "app.py",
+  "movie.avi",
+  "song.wav",
+  "backup.tar.gz",
+  "random.xyz",
+  "nodejs.zip",
 ];
 
+function intializeDirectories() {
+  if (!fs.existsSync(sourceDir)) {
+    fs.mkdirSync(sourceDir, { recursive: true });
 
-function intializeDirectories () {
-    if(!fs.existsSync(sourceDir)){
-        fs.mkdirSync(sourceDir, {recursive: true});
-
-        testFiels.forEach((file) => {
-            fs.writeFileSync(path.join(sourceDir, file), `Content of ${file}`)
-        })
-    }
-
-    console.log("Messy directories files are created!!!");
-
-
-    if(!fs.existsSync(organizeDir)){
-        fs.mkdirSync(organizeDir, {recursive: true})
-    }
-    Object.keys(categories).forEach((category)=> {
-        const categoryPath = path.join(organizeDir, category);
-        if(!fs.existsSync(categoryPath)){
-            fs.mkdirSync(categoryPath)
-        }
+    testFiels.forEach((file) => {
+      fs.writeFileSync(path.join(sourceDir, file), `Content of ${file}`);
     });
+  }
+
+  console.log("Messy directories files are created!!!");
+
+  if (!fs.existsSync(organizeDir)) {
+    fs.mkdirSync(organizeDir, { recursive: true });
+  }
+  Object.keys(categories).forEach((category) => {
+    const categoryPath = path.join(organizeDir, category);
+    if (!fs.existsSync(categoryPath)) {
+      fs.mkdirSync(categoryPath);
+    }
+  });
 }
 
 // intializeDirectories()
 
-function getCategory (fileName) {
-    const ext = path.extname(fileName).toLowerCase(); // ".pdf", ".jpg"
+function getCategory(fileName) {
+  const ext = path.extname(fileName).toLowerCase(); // ".pdf", ".jpg"
 
-    // [images: [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg"]],
+  // [images: [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg"]],
 
-    for(let [category, extentions] of Object.entries(categories)){
-        if(extentions.includes(ext)){
-            return category
-        }
+  for (let [category, extentions] of Object.entries(categories)) {
+    if (extentions.includes(ext)) {
+      return category;
     }
-    return "others";
+  }
+  return "others";
+}
 
+function organizerFile() {
+  console.log("fiel organizer \n");
+  console.log("Source", sourceDir);
+  console.log("Destination", organizeDir);
+  console.log("\n" + "-".repeat(50) + "\n");
+
+  const files = fs.readdirSync(sourceDir);
+  if (files.length === 0) {
+    console.log("No files to work on!!");
+    return;
+  }
+  console.log(`found ${files.length} files to  organize \n`);
+  const stats = {
+    total: 0,
+    byCategroy: {},
+  };
+
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceDir, file);
+    const stat = fs.statSync(sourcePath);
+    if (stat.isDirectory()) {
+      return;
+    }
+
+    const category = getCategory(file);
+    const destDir = path.join(organizeDir, category);
+    const destPath = path.join(destDir, file);
+
+    fs.copyFileSync(sourcePath, destPath);
+
+    stats.total++;
+    stats.byCategroy[category] = (stats.byCategroy[category] || 0) + 1;
+    console.log(`${file}`);
+    console.log(`${category}`);
+    console.log(`${stat.size}`);
+  });
+}
+
+function showHelp() {
+  console.log(`
+          file organizer - usage: 
+
+          commands:
+          init - create files
+          organize - organize fiels into categories
+
+          example:
+          node fiel-organizer init
+          node file-organizer organize
+        `);
+}
+
+const command = process.argv[2];
+switch (command) {
+  case "init":
+    intializeDirectories();
+    break;
+  case "organize":
+    organizerFile();
+    break;
+  default:
+    showHelp();
+    break;
 }
